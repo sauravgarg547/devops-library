a962898ace3502df1a7ae48b923eaedd718d1355
Saurav Garg <72258364+sauravgarg547@users.noreply.github.com>
2024-11-15 03:12:12 +0530

    Create pushDockerImage.groovy

---
+++ vars/pushDockerImage.groovy
@@ -0,0 +1,11 @@
+// vars/pushDockerImage.groovy
+def call(String sourceImage, String targetImage) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCreds', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]) {
+        sh """
+            docker login -u ${dockerHubUser} -p ${dockerHubPass}
+            docker tag ${sourceImage} ${dockerHubUser}/${targetImage}
+            docker push ${dockerHubUser}/${targetImage}
+        """
+        echo "Successfully pushed ${targetImage} to DockerHub"
+    }
+}
